11171bb2357ee8921eabab5b1d9f4f1f787e2101
Gurpreet <san22688@spengergasse.at>
2022-05-29 13:56:10 +0200

    customer1

---
+++ src/customer_sandhu/customer.java
@@ -0,0 +1,57 @@
+package customer_sandhu;
+
+public class customer {
+	private int customer_id;
+	private String email;
+	private String firstname;
+	private String lastname;
+	private boolean gender;
+	private String birthday;
+	private double credits;
+	public int getCustomer_id() {
+		return customer_id;
+	}
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public boolean isGender() {
+		return gender;
+	}
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public double getCredits() {
+		return credits;
+	}
+	public void setCredits(double credits) {
+		this.credits = credits;
+	}
+
+	
+	
+	
+}
